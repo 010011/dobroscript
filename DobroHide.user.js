0024abbac534da791c8d473d684c24ae06ec8c3f
010011 <010011@2ch.hk>
2013-04-28 19:48:58 +0400

    Добавлены первые версии скрывалки тредов DobroHide и добавителя ответов DobroResponse

---
+++ DobroHide.user.js
@@ -0,0 +1,137 @@
+// ==UserScript==
+// @name        DobroHide
+// @namespace   https://github.com/010011/dobroscript
+// @description Долой мусор! Контролируйте отображаемые треды по их заголовкам.
+// @include     http://dobrochan.ru/*
+// @grant       GM_registerMenuCommand
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @version     1.0
+// @require     http://zeptojs.com/zepto.min.js
+// ==/UserScript==
+
+GM_registerMenuCommand("Настройка тредов", openInterface);
+var HIDDEN_THREADS = loadSettings();
+
+// close settings window and apply changes
+function closeSettings() {
+    var HIDDEN_THREADS = loadSettings();
+    $('.rage_settings').remove();
+    hideThreads(HIDDEN_THREADS);
+}
+
+// write settings to greasemonkey's variables
+function saveSettings() {
+    var threads = $('.rage_threads').val().split("\n");
+    GM_setValue('rage_threads', JSON.stringify(threads));
+    $('.rage_confirm').text('Схоронено').animate(
+        {opacity:0},
+        {
+            duration:1000, complete:function(){
+                $('.rage_confirm').text('').css('opacity',100);
+            }
+        }
+    );
+}
+
+// load array of threads to hide from greasemonkey's variables
+function loadSettings() {
+    var threads = GM_getValue('rage_threads', false);
+    if (threads) {
+        return JSON.parse(threads);
+    }
+    else {
+        return "Впишите сюда заголовки тредов — по одному на линию.\n" +
+        "Например:\nПонитред\nРандомная пикча-тред\nБессоница-тред";
+    }
+}
+
+// convert array to newline-separated text
+function array2text(a) {
+    var t = '';
+    for (i in a) {
+        t = t + a[i] + "\n";
+    }
+    return t.substring(0, t.length-1);
+}
+
+// open settings window and show hidden threads' titles
+function openInterface() {
+    var HIDDEN_THREADS = loadSettings();
+
+    var settings = document.createElement('div');
+    $(settings).addClass('rage_settings')
+    $(settings).css({
+        'z-index': '99999',
+        'position': 'fixed',
+        'top': '10px',
+        'left': '50%',
+        'width': '400px',
+        'height': '460px',
+        'padding': '10px',
+        'margin-left': '-200px',
+        'background-color': '#000'
+    });
+    $(settings).html('<h3 style="color: #fff">Автоматически скрывать:</h3>');
+
+    var area = document.createElement('textarea');
+    $(area).addClass('rage_threads');
+    $(area).css({
+        'width': '390px',
+        'height': '340px',
+        'margin-left': '0px',
+        'margin-right': '5px',
+        'margin-bottom': '10px'
+    });
+    var threads = array2text(HIDDEN_THREADS);
+    $(area).text(threads);
+    $(area).appendTo($(settings));
+
+    var save = document.createElement('a');
+    $(save).html('Схоронить');
+    $(save).css({
+        'color': '#f80'
+    });
+    $(save).click(function(){saveSettings();});
+    $(save).appendTo($(settings));
+
+    var close = document.createElement('a');
+    $(close).html('Закрыть');
+    $(close).css({
+        'margin-left': '200px',
+        'color': '#f80'
+    })
+    $(close).click(function(){closeSettings();});
+    $(close).appendTo($(settings));
+
+    $(settings).append('<br><span class="rage_confirm"></span>');
+    $(settings).appendTo($('body'));
+};
+
+// add threads to doborchan's cookie and physicaly remove them
+function hideThreads(hidden_threads) {
+    var ops = $('.oppost label .replytitle');
+    $.each(ops, function(key, thread) {
+        var title = $(thread).text();
+        if ($.inArray(title, hidden_threads) != -1) {
+            var href = $(thread).parent().find('.hide').attr('href');
+            var victim = $(thread).parent().parent().parent();
+            victim.hide(); //thread
+            victim.next().hide(); // break
+            victim.next().next().hide(); // hr
+            $.ajax({
+                type: 'GET',
+                url: 'http://dobrochan.ru' + href,
+                success: function(){
+                    console.log('Hidden: ' + title);
+                }
+            });
+        }
+    });
+}
+
+/**********************************************************************/
+
+$(document).ready(function(){
+    hideThreads(HIDDEN_THREADS);
+});
---
+++ DobroResponse.user.js
@@ -0,0 +1,66 @@
+// ==UserScript==
+// @name        DobroResponse
+// @namespace   https://github.com/010011/dobroscript
+// @description Добавляет список ответов к посту
+// @include     http://dobrochan.ru/*
+// @grant       none
+// @version     1
+// @require     http://zeptojs.com/zepto.min.js
+// ==/UserScript==
+
+// generate reply link with popup
+function reply_link(to_post, from_post) {
+    var a = document.createElement('a');
+    $.each( to_post.attributes, function( index, attr ) {
+        if (attr.name == 'onmouseover') {
+            //  FUCK LOR
+        }
+    } );
+    // ) and #i are dirty hacks for valid links in op post
+    // Узнать, почему на лоре крешится onmouseover
+    var onMouse = $(to_post).attr('onmouseover').replace($(to_post).text().substring(2) + ')', from_post  + ')');
+    var href =    $(to_post).attr('href').replace('#i' + $(to_post).text().substring(2), '#i' + from_post);
+    $(a).attr({
+        'onMouseOver': onMouse,
+        'href': href
+    });
+    $(a).text('>>' + from_post);
+    return $(a);
+}
+
+// append reply link to post body
+function append_reply(to_post, from_post) {
+    var post = $('#post_' + $(to_post).text().substring(2) + ' .postbody .message');
+    var replies = post.find('p.replies');
+    // check if post already has reply block
+    if (replies.length > 0) {
+        $(replies).append('&nbsp;', reply_link(to_post, from_post));
+    }
+    else {
+        // create reply block in post body
+        var replies = document.createElement('p');
+        $(replies).addClass('replies');
+        $(replies).css({
+            'margin-top': '15px',
+            'font-size': '12px',
+            'color': '#444'
+        });
+        $(replies).html('Ответы:');
+        $(replies).append('&nbsp;', reply_link(to_post, from_post));
+        $(replies).appendTo(post);
+    }
+}
+
+$(document).ready(function(){
+    // all messages on page
+    var posts = $('.message');
+    $.each(posts, function(ind, post){
+        // current post number
+        var from_post = $(post).parent().parent().find('a').attr('name').substring(1);
+        // links to previous posts
+        var links = $(post).find('a:contains(">>")');
+        $.each(links, function(ind, to_post) {
+            append_reply(to_post, from_post);
+        });
+    });
+});
